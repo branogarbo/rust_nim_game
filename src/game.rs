312a85cfb3b3d81954e99bfca2eea38d86fac561
Brian Longmore <branodev@gmail.com>
2021-11-08 20:24:58 -1000

    lot o stuff, stable

--- src/game.rs
+++ src/game.rs
@@ -1,35 +1,43 @@
 use rand::Rng;
-use std::io;
+use std::*;
 
 pub struct Nim {
-   obj_count: i8,
-   player_first: bool,
+   nim_count: i8,
+   user_first: bool,
    user_won: bool,
+   game_done: bool,
 }
 impl Nim {
-   pub fn new(obj_count: i8, player_first: bool) -> Self {
+   pub fn new(nim_count: i8, user_first: bool) -> Self {
       Self {
-         obj_count,
-         player_first,
+         nim_count,
+         user_first,
          user_won: false,
+         game_done: false,
       }
    }
 
    pub fn play(&mut self) {
-      if self.player_first {
+      println!("There are {} nims on the table!", self.nim_count);
+      println!("----------------------------------------");
+
+      if self.user_first {
          self.user_player_turn();
       }
 
-      while self.obj_count > 0 {
+      loop {
          self.other_player_turn();
+         if self.game_done {
+            break;
+         }
+
          self.user_player_turn();
+         if self.game_done {
+            break;
+         }
       }
 
-      if self.user_won {
-         println!("Congratulations, you won!");
-      } else {
-         println!("Awww better luck next time!");
-      }
+      self.game_over();
    }
 
    fn user_player_turn(&mut self) {
@@ -38,26 +46,65 @@ impl Nim {
       match get_input_i8() {
          Ok(c) => {
             if c < 1 || c > 4 {
-               panic!("⚠️  Please enter a valid input ⚠️"); // try to find better way, error handling
+               println!("Please enter a valid input!");
+               self.user_player_turn();
+               return;
             }
 
-            self.obj_count -= c;
+            self.nim_count -= c;
 
-            if self.obj_count <= 0 {
+            println!("You took {} from the table!", c);
+            println!("----------------------------------------");
+
+            if self.nim_count <= 0 {
                self.user_won = true;
+               self.game_done = true;
             }
+
+            println!("New count: {}", self.nim_count);
          }
-         Err(e) => {
-            panic!("{}", e);
+         Err(_) => {
+            println!("Please enter a valid input!");
+            self.user_player_turn();
+            return;
          }
       }
    }
 
    fn other_player_turn(&mut self) {
-      // let ran_take_num = radn
+      println!("Your opponent is making a move...");
+
+      let one_sec = time::Duration::from_millis(1000);
+      thread::sleep(one_sec);
+
+      let take_num: i8;
+      if self.nim_count < 5 {
+         take_num = self.nim_count;
+      } else {
+         take_num = rand::thread_rng().gen_range(1..=4);
+      }
+      self.nim_count -= take_num;
+
+      println!("Your opponent took {} from the table!", take_num);
+
+      thread::sleep(one_sec);
+
+      println!("----------------------------------------");
+
+      if self.nim_count == 0 {
+         self.game_done = true;
+      }
+
+      println!("New count: {}", self.nim_count);
    }
 
-   fn game_over() {}
+   fn game_over(&self) {
+      if self.user_won {
+         println!("Congratulations, you won!");
+      } else {
+         println!("Awww better luck next time!");
+      }
+   }
 }
 
 fn get_input_string() -> String {
@@ -73,6 +120,6 @@ fn get_input_i8() -> Result<i8, String> {
 
    match i.parse() {
       Ok(num) => return Ok(num),
-      Err(_) => return Err("⚠️  Please enter a valid input ⚠️".to_string()),
+      Err(_) => return Err("Please enter a valid input!".to_string()),
    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod game;
 use game::Nim;
 
 fn main() {
-    let mut game = Nim::new(15, true);
+    let mut game = Nim::new(15, false);
 
     game.play();
 }
